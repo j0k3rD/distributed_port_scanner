c33406a7bddefb17a53599e2dbf0c668e3e9086a
j0k3r-Desktop <agm.moya@alumno.um.edu.ar>
2023-01-29 20:06:48 -0300

    starting w celery

--- views.py
+++ views.py
@@ -2,17 +2,31 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from . import app
 from services.port_scanning import *
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/scan', methods=['GET', 'POST'])
+def scan():
     if request.method == 'POST':
         ip = request.form['ip']
         port = request.form['port']
         option = request.form['option']
         if option == '1':
-            open_ports = scan_with_python(ip, port)
+            scan_with_python.delay(ip, port)
         elif option == '2':
-            open_ports = scan_with_nmap(ip, port)
+            scan_with_nmap.delay(ip, port)
         else:
             return 'Invalid option'
-        return render_template('index.html', open_ports=open_ports)
-    return render_template('index.html')
+        return redirect(url_for('index'))
+    return render_template('scan.html')
+
+@app.route('/scan/<ip>/<port>/<option>')
+def scan_with_params(ip, port, option):
+    if option == '1':
+        scan_with_python.delay(ip, port)
+    elif option == '2':
+        scan_with_nmap.delay(ip, port)
+    else:
+        return 'Invalid option'
+    return redirect(url_for('index'))
